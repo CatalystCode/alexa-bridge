e8baa68ab8a24dad7e03f7be3cdbc5db02454172
tobe <tobybrad@microsoft.com>
2017-01-24 12:38:39 +0000

    Reduce logging

--- server.js
+++ server.js
@@ -144,8 +144,6 @@ function startBridge() {
   server.use(restify.bodyParser());
   server.post('/messages', (req, res, err) => alexaSays(req, res, connector, err) );
 
-  console.log("STARTING");
-  console.log(process.env);
   server.listen(process.env.port || process.env.PORT || 8080, function() {
     console.log('%s listening at %s', server.name, server.url);
   });
